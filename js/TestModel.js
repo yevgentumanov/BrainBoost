7128dbe765b7321ab5651d942a2cce3998938c36
sfbluebell <5634224@alu.murciaeduca.es>
2023-04-16 21:18:35 +0200

    Buen push, contrato entre backend y frontend Este push ha consistido en la creación de más ejemplos de cómo deben llegar los datos desde la parte del entorno servidor, esto es, para establecer un "contrato" a modo de pautas de cómo debe comunicarse cliente y servidor. Como se puede observar, ha sido cambiado, y el TestModel también ha sufrido bastantes modificaciones. Aún así, este push no ha acabado, ahora después continuaré para dejar terminada la clase del TestModel.

--- js/TestModel.js
+++ js/TestModel.js
@@ -1,16 +1,21 @@
 /*=====================================
             IMPORTACIONES
  =====================================*/
+import * as globals from './globals.json';
+// import * as irregular from './example_rellenar_huecos.json';
 import * as apij from './json.js';
 import * as utilities from './utilidades.js';
 
 /*====================================
             ENUMERADOS
 ======================================*/
-const TipoTest = {
-    NONE: 0,
-    MULTIPLE_CHOICE: 1,
-    UNIQUE_RESPONSE: 2
+const TipoPregunta = {
+    NONE: 0, // No se ha definido el tipo de pregunta
+    MULTIPLE_RESPONSE: 1, // Múltiples respuestas, única opción correcta
+    MULTIPLE_RESPONSE_MULTIPLE_CHOICE: 2, // Múltiples respuestas, de múltiple elección (varias respuestas correctas posibles)
+    UNIQUE_RESPONSE: 3, // Escribir la respuesta en una caja de texto
+    FILL_IN_GAPS: 4, // Rellenar huecos (hay que rellenarlos todos)
+    FILL_GAPS_GIVEN_ONE: 5 // Rellenar huecos dado uno (Ejemplo: verbos irregulares)
 }
 
 /*====================================
@@ -20,20 +25,59 @@ export class Test {
     /**
      * Constructor para crear un objeto de tipo test, que contendrá un test recogido de la BB.DD de la aplicación.
      * @param {number} idTest Especifica el id del test en la BB.DD.
-     * @param {object} preguntas Array que contiene las preguntas del test y respuestas a cada una de ellas, así como su respuesta correcta.
+     * @param {Array} preguntas Objeto JSON que contiene las preguntas del test y respuestas a cada una de ellas, así como su respuesta correcta.
+     * @param {string} categoriaTest Especifica la categoría/materia a la que pertenece el test.
+     * 
      */
-    constructor(idTest, preguntas, length = null, tipoTest = TipoTest.NONE) {
-        if (typeof(idTest) == "number", typeof(preguntas) == "object") {
-            /*-- Atributos de la clase --*/
+    constructor(idTest = null, preguntas = null, categoriaTest = null) {
+        /*====================================================
+              Define todos los atributos de la clase
+        =====================================================*/
+        this.id_test = null; // (number) (se rescata de la BB.DD / se puede inventar desde el constructor con fines de pruebas internas del equipo de desarrollo, no se puede crear (cuando estemos creando un test mediante el creador de tests que desarrollaremos))
+        this.length = null; // (number) (se rescata de la BB.DD / se rescata del objeto preguntas (cuando se esté creando un test con el creador de test que desarrollaremos))
+        this.preguntas = null; // (array de objetos JSON) (se rescatan de la BB.DD / se pueden crear, modificar y eliminar con métodos específicos para ello)
+
+        this.nombre_test = null; // (string) (se rescata de la BB.DD / se asigna con el setter desde el controlador)
+        this.descripcion = null; // (string) (se rescata de la BB.DD / se asigna con el setter desde el controlador)
+        this.numero_visitas = null; // (number) (solo se rescata de la BB.DD)
+
+        this.id_usuario_creador = null; // (number) (se rescata de la BB.DD / no se puede asignar de otra forma, cuando un usuario cree un test desde el creador de tests, será desde el backend desde donde se reciba el id del usuario y se le asignará allí antes de guardar los datos en la BB.DD. Pero desde el frontend partirá con valor null)
+        this.nombre_usuario_creador = null; // (string) (se rescata de la BB.DD / no se puede asignar de otra forma, cuando un usuario cree un test desde el creador de tests, será desde el backend desde donde se reciba el id del usuario y se le asignará allí antes de guardar los datos en la BB.DD. Pero desde el frontend partirá con valor null)
+
+        this.id_materia = null; // (number) (se rescata de la BB.DD / se asigna con el setter desde el controlador, cuando el usuario esté creando un test en el creador de tests)
+        this.nombre_materia = null; // (string) (se rescata de la BB.DD / se asigna con el setter desde el controlador, cuando el usuario esté creando un test en el creador de tests)
+
+        this.id_usuario = null; // (number) (se rescata de la BB.DD desde el servidor)
+        this.nota = null; // (number) (se rescata de la BB.DD / se asigna con el setter desde el controlador)
+        this.fecha_realizacion = null; // (Date) (se rescata de la BB.DD / se asigna con el setter desde el controlador)
+        
+        /*===========================================
+                Rellena todos los parámetros
+        ============================================*/
+        /*-- Rellena los atributos en función de los parámetros que reciba el constructor --*/
+        if (typeof(idTest) == "number") {
+            /*-- Atributos del test en sí --*/
             this.id_test = idTest;
-            if (length == null) {
-                this.length = preguntas.length;
-            } else {
-                this.length = length;
-            }
-            this.preguntas = preguntas;
-            this.tipoTest = tipoTest;
         }
+
+        /*-- Rellena los atributos de preguntas y length --*/
+        if (preguntas && typeof(preguntas) == "object" && preguntas instanceof Array) {
+            this.preguntas = preguntas;
+            this.length = preguntas.length;
+        } else {
+            this.length = 0;
+        }
+
+        /*-- Más atributos del test en sí --*/
+        if (nombreTest) {
+            this.nombre_test = nombreTest;
+        } else {
+            this.nombre_test = null;
+        }
+
+        /*-- Atributos relacionados con el usuario que lo realiza --*/
+        this.nota; // Nota que ha sacado el usuario en ese test
+        this.fecha_realizacion;
     }
 
     /**
@@ -60,8 +104,108 @@ export class Test {
         return this.preguntas[idPregunta];
     }
 
-    getTipoTest() {
-        return this.tipoTest;
+    /**
+     * Método para añadir una pregunta al test
+     */
+    addPregunta(pregunta) {
+        this.preguntas.push(pregunta);
+    }
+
+    /**
+     * Método que permite modificar una pregunta.
+     */
+    modifyPregunta(idPregunta, pregunta) {
+        this.preguntas[idPregunta] = pregunta;
+    }
+
+    /**
+     * Método que sirve para eliminar una pregunta dentro del objeto test.
+     * @param {number} idPregunta Especifica el id de la pregunta (id numérico para identificar dentro del array de preguntas)
+     */
+    removePregunta(idPregunta) {
+        this.preguntas.splice(idPregunta, 1);
+    }
+
+    /**
+     * Método que sirve para establecer el nombre del test.
+     * @param {string} nombreTest Especifica el nombre del test.
+     */
+    setNombreTest(nombreTest) {
+        this.nombre_test = nombreTest;
+    }
+
+    /**
+     * Método que devuelve el nombre del test.
+     * @returns El nombre del test.
+     */
+    getNombreTest() {
+        return this.nombre_test;
+    }
+
+    /**
+     * Método que sirve para establecer la descripción del test.
+     * @param {string} descripcionTest Especifica la descripción del test.
+     */
+    setDescripcion(descripcionTest) {
+        this.descripcion = descripcionTest;
+    }
+
+    /**
+     * Método que devuelve la descripción del test.
+     * @returns La descripción del test.
+     */
+    getDescripcionTest() {
+        return this.descripcion;
+    }
+
+    /**
+     * Método que devuelve el número de visitas que ha tenido del test.
+     * @returns El número de visitas que ha tenido del test.
+     */
+    getNumeroVisitas() {
+        return this.descripcion;
+    }
+
+    /**
+     * Método que devuelve el número de visitas que ha tenido del test.
+     * @returns El número de visitas que ha tenido del test.
+     */
+    getNumeroVisitas() {
+        return this.descripcion;
+    }
+
+
+
+    /**
+     * Descarga información acerca del test.
+     */
+    downloadInfoAboutTestByIdTest(id_usuario = null) {
+
+    }
+
+    /**
+     * Descarga del servidor las 10 siguientes preguntas (y sus respuestas).
+     * @throws {Error} Puede lanzar un error si no consigue descargar la información de las preguntas del servidor.
+     */
+    downloadQuestionsByIdTest() {
+        // irregular[0].tabla_preguntas[0]['Forma base']
+        /*-- Creación de un array de Tests --*/
+        let preguntas = Array(); // Aquí se almacenarán las preguntas del test que se recojan del servidor
+    
+        /*-- Obtiene los datos del servidor --*/
+        apij.obtenerJSON(globals.constantes.HOST_NAME + globals.constantes.RUTA_PREGUNTAS, "GET", null, {idTest: this.id_test, diezPreguntasHasta: 10})
+        /* diezPreguntasHasta es un parámetro que se le pasa al servidor para indicarle que, por ejemplo, si estamos en viendo las preguntas del 1-10 y queremos ver las siguientes 10,
+        se lo especificamos diciéndole que diezPreguntasHasta = 20, porque serían 10 preguntas desde la pregunta número 10 (que es la última visible en la página hasta el momento) */
+        .then(response => {
+    
+        }).catch(error => {
+            /*-- Descarta que haya dado error --*/
+            throw new Error("Se ha producido un error al intentar descargar la información de las preguntas del servidor. Mensaje de error: " + error.message);
+        });
+        
+        
+        /*-- Devuelve el resultado de la variable tests (el controlador se encargará de verificar si esto ha devuelto realmente la lista de tests o un error) --*/
+        return tests;
     }
 }
 
@@ -72,24 +216,10 @@ export class Test {
 /**
  * (Método implementado solo parcialmente).
  * @param {object} test (Obligatorio) Especifica un objeto test, por si hubiera simplemente que cargar más preguntas de un mismo test (en caso de que tuviera más de 10 preguntas, ya que de lo contrario, se cargan todas de una sola vez y no sería necesario invocar a este método).
- * @returns Devuelve
+ * @returns Devuelve el objeto test, con las nuevas preguntas cargadas.
  */
 function obtenerPreguntasTest(test = null) {
-    /*-- Creación de un array de Tests --*/
-    let preguntas = Array(); // Aquí se almacenarán las preguntas del test que se recojan del servidor
-
-    /*-- Obtiene los datos del servidor --*/
-    apij.obtenerJSON("http://localhost/preguntas", "GET", null, {idTest: this.id_test, diezPreguntasHasta: 10})
-    /* diezPreguntasHasta es un parámetro que se le pasa al servidor para indicarle que, por ejemplo, si estamos en viendo las preguntas del 1-10 y queremos ver las siguientes 10,
-    se lo especificamos diciéndole que diezPreguntasHasta = 20, porque serían 10 preguntas desde la pregunta número 10 (que es la última visible en la página hasta el momento) */
-    .then(response => {
-
-    }).catch(error => {
-
-    });
-
-    /*-- Devuelve el resultado de la variable tests (el controlador se encargará de verificar si esto ha devuelto realmente la lista de tests o un error) --*/
-    return tests;
+    
 }
 
 /**
